a027aa8df47af55a6885c375f000224cbf40f2d3
Mundt-Petersen <walter.mundt.petersen737@gmail.com>
2026-03-17 15:44:36 +0100

    lab 2 cleanup

--- lab2/intopt.c
+++ lab2/intopt.c
@@ -99,27 +99,7 @@ double init(simplex_t *s, int m, int n, int *var, double **a, double *b, double 
         return k;
     }
 }
-void prepare(simplex_t *s, int k)
-{
-    int m = s->m;
-    int n = s->n;
-    int i;
-    for (i = m + n; i > n; i++)
-    {
-        s->var[i] = s->var[i - 1];
-    }
-    s->var[n] = m + n;
-    n++;
-    for (i = 0; i < m; i++)
-    {
-        s->a[i][n - 1] = -1;
-    }
-    s->x = calloc(m + n, sizeof(double));
-    s->c = calloc(n, sizeof(double));
-    s->c[n - 1] = 1;
-    s->n = n;
-    pivot(s, k, n - 1);
-}
+
 int initial(simplex_t *s, int m, int n, double **a, double *b, double *c, double *x, double y, int *var)
 { // assume bi ≥ 0 so skip the call to prepare and the rest of initial
     int i, j, k;
@@ -129,105 +109,7 @@ int initial(simplex_t *s, int m, int n, double **a, double *b, double *c, double
     {
         return 1;
     }
-    /*
-        prepare(s, k);
-        n = s->n;
-        s->y = xsimplex(m, n, s->a, s->b, s->c, s->x, 0, s->var, 1);
-        for (i = 0; i < m + n; i++)
-        {
-            if (s->var[i] = m + n - 1)
-            {
-                if (abs(s->x[i]) > EPSILON)
-                {
-                    free(s->x);
-                    free(s->c);
-                    return 0;
-                }
-                else
-                {
-                    break; // This i will be used on the next page.
-                }
-            }
-        }
-        if (i >= n)
-        {
-            // x n+m is basic. find good nonbasic.
-            for (j = k = 0; k < n; k++)
-            {
-                if (abs(s->a[i - n][k]) > abs(s->a[i - n][j]))
-                {
-                    j = k;
-                    pivot(s, i - n, j);
-                    i = j;
-                }
-            }
-        }
-        if (i < n - 1)
-        {
-            // x n+m is nonbasic and not last. swap columns i and n-1
-            k = s->var[i];
-            s->var[i] = s->var[n - 1];
-            s->var[n - 1] = k;
 
-            for (k = 0; k < m; k = k + 1)
-            {
-                w = s->a[k][n - 1];
-                s->a[k][n - 1] = s->a[k][i];
-                s->a[k][i] = w;
-            }
-        }
-        else
-        {
-            // x n+m is nonbasic and last. forget it.
-            free(s->c);
-            s->c = c;
-            s->y = y;
-        }
-        for (k = n - 1; k < n + m - 1; k++)
-        {
-            s->var[k] = s->var[k + 1];
-        }
-
-        s->n = s->n - 1;
-        n = s->n;
-        double *t = calloc(n, sizeof(double));
-
-        for (k = 0; k < n; k = k + 1)
-        {
-            for (j = 0; j < n; j = j + 1)
-            {
-                if (k = s->var[j])
-                {
-                    // xk is nonbasic. add ck
-                    t[j] = t[j] + s->c[k];
-                    goto next_k;
-                    // xk is basic.
-                }
-            }
-            for (j = 0; j < m; j = j + 1)
-            {
-                if (s->var[n + j] = k)
-                {
-                    // xk is at row j
-                    break;
-                }
-            }
-            s->y = s->y + s->c[k] * s->b[j];
-            for (i = 0; i < n; i = i + 1)
-            {
-                t[i] = t[i] - s->c[k] * s->a[j][i];
-            }
-        next_k:;
-        }
-        for (i = 0; i < n; i = i + 1)
-        {
-            s->c[i] = t[i];
-        }
-        // delete t and s.x
-        free(t);
-        free(s->x);
-        return 1;
-        */
 }
 
 int select_nonbasic(simplex_t *s)
@@ -314,7 +196,7 @@ int simplex(int m, int n, double **a, double *b, double *c, double *x, double y)
 
 
 
-int glob = 0;
+
 int main(void)
 {
     int m;
@@ -324,7 +206,6 @@ int main(void)
     double *c;
     double *x;
     size_t i;
-    glob+=1;
     //
     // Scan input
     //
